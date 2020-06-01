42425485df3551b725d8a64c1fed9d4684789247
mcairnsa <madison.cairns.14@gmail.com>
2020-05-31 22:03:13 -0500

    PageParser Tests

--- src/test/java/edu.depaul.email/PageParserTest.java
+++ src/test/java/edu.depaul.email/PageParserTest.java
@@ -1,4 +1,55 @@
 package edu.depaul.email;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class PageParserTest {
+
+    @Test
+    @DisplayName("tests html without <a> tags")
+    void ParserATest() {
+        String html = "href='/some/other/file.html'my link/a/body/html";
+        Document doc = Jsoup.parse(html);
+        PageParser parser = new PageParser();
+        parser.findLinks(doc);
+    }
+
+    @Test
+    @DisplayName("tests html with only one <a> tags")
+    void ParserOneATest() {
+        String html = "<a href='/some/other/file.html'> my link/a/body/html</a>";
+        Document doc = Jsoup.parse(html);
+        PageParser parser = new PageParser();
+        parser.findLinks(doc);
+    }
+
+    @Test
+    @DisplayName("tests html with multiple <a> tags")
+    void ParserMultiATest() {
+        String html = "<a> <html> '/some/other/file.html'> my link/a/body/html</a>";
+        Document doc = Jsoup.parse(html);
+        PageParser parser = new PageParser();
+        parser.findLinks(doc);
+    }
+
+    @Test
+    @DisplayName("tests PageParser with relative tags")
+    void ParserRelativeTest() {
+        String html = "<a other/file'> my link/a/body/html</a>";
+        Document doc = Jsoup.parse(html);
+        PageParser parser = new PageParser();
+        parser.findLinks(doc);
+    }
+
+    @Test
+    @DisplayName("tests PageParser with absolute tags")
+    void ParserAbsoluteTest() {
+        String html = "<a https://www.example.com/other/file'> my link/a/body/html</a>";
+        Document doc = Jsoup.parse(html);
+        PageParser parser = new PageParser();
+        parser.findLinks(doc);
+    }
+
 }
