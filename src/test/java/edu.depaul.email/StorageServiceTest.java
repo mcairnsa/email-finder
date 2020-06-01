2cabba43d206400e39ab2aed67e5e01ad877cabc
mcairnsa <madison.cairns.14@gmail.com>
2020-05-31 20:51:27 -0500

    StorageService Tests

--- src/test/java/edu.depaul.email/StorageServiceTest.java
+++ src/test/java/edu.depaul.email/StorageServiceTest.java
@@ -1,4 +1,37 @@
 package edu.depaul.email;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class StorageServiceTest {
+
+    @Test
+    @DisplayName("tests that addLocation() and storeList() work properly, sent to email.txt")
+    void StorageServiceLocationTest() {
+       StorageService x = new StorageService();
+       ArrayList<String> lst = new ArrayList<String>();
+       lst.add("HelloWorld@gmail.com");
+       lst.add("testTEST@msn.com");
+       lst.add("HiThere@aol.com");
+       lst.add("_-o_o-_@yahoo.com");
+       x.addLocation(StorageService.StorageType.EMAIL ,"email.txt");
+       x.storeList(StorageService.StorageType.EMAIL, lst);
+    }
+
+    @Test
+    @DisplayName("tests that addLocation() and storeList() send error messages when path isn't valid")
+    void StorageServiceFailTest() {
+        StorageService x = new StorageService();
+        ArrayList<String> lst = new ArrayList<String>();
+        lst.add(":(.email.com");
+        lst.add("D:.notarealemail.com");
+        x.addLocation(StorageService.StorageType.EMAIL ,"email-finder/falsePath");
+        x.storeList(StorageService.StorageType.EMAIL, lst);
+    }
+
 }
